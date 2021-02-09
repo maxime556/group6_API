efda27cf517c9cbd5c72579758c9693280748a75
mtounekti <mtounekti@simplonprod.co>
2021-02-09 14:59:08 +0100

    changed config

--- src/service/repository/track.repository.ts
+++ src/service/repository/track.repository.ts
@@ -3,12 +3,12 @@ import { Track } from '../../model/Track'
 
 class TrackRepositoryService {
 
-    getTracks(limit: number = 10 , offset: number = 0): Promise<Track> {
+    getTracks(limit: number = 10 , offset: number = 0):  Promise<Array<Track>> {
 
         const queryString = `SELECT id , title, album, band FROM track LIMIT ${limit} OFFSET ${offset}`
         return new Promise((resolve, reject) => {
             DB.query(queryString)
-                .then((results: any) => {
+                .then((results: Array<any>) => {
                     resolve(results.map(row => new Track(row)))
                 }).catch(e => {
                     reject(e)
