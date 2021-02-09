04fe4a83fb522c29093ad98f7c5442a13aa0cea7
mtounekti <mtounekti@simplonprod.co>
2021-02-09 15:51:03 +0100

    fix

--- pre-test.sh
+++ pre-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cat ./test/create_db.sql | mysql -h127.0.0.1 -u marouatnk -p password
-cat ./test/mockup_db.sql | mysql -h127.0.0.1 -u marouatnk -p password deploy_test
+cat ./test/create_db.sql | mysql -h127.0.0.1 -umarouatnk -ppassword
+cat ./test/mockup_db.sql | mysql -h127.0.0.1 -umarouatnk -ppassword deploy_test
