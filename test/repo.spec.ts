db05ad0c2d783e3965bc51220d4e738c70fca28f
mtounekti <mtounekti@simplonprod.co>
2021-02-09 15:26:50 +0100

    feat: config test container

---
+++ test/repo.spec.ts
@@ -0,0 +1,26 @@
+import * as path from "path"
+import * as dotenv from "dotenv"
+import * as chai from "chai"
+import * as assert from "assert"
+const expect = chai.expect
+
+const envPath = path.resolve(__dirname + "/../.env.test")
+dotenv.config({ path: envPath })
+
+import { TrackRepository } from "../src/service/repository/track.repository"
+
+describe("Test user repository", () => {
+    
+    it("Test if getUser returns correct results", () => {
+        
+        return TrackRepository.getTracks(3, 0)
+            .then((results) => {
+                expect(results.length).to.equal(3)
+            })
+            .catch(e => {
+                console.log(e)
+                assert.fail(e)
+            })
+
+    })
+})
