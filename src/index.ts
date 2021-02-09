2be49d19edc0eb07cc81213aa33138cc0a2faf02
mtounekti <mtounekti@simplonprod.co>
2021-02-09 09:28:49 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,27 @@
-import { Args } from "./config/args"
+//import { Args } from "./config/args"
+require('dotenv').config()
+
 import { Config } from "./config/config.service"
 import { DB } from "./service/db.service"
 
-Args.required("env", ["local", "test", "beta", "prod"])
-const ENV = Args.get("env")
+console.log(process.env)
 
-Config.init(ENV)
+//Args.required("env", ["local", "test", "beta", "prod"])
+//const ENV = Args.get("env")
+////console.log(`Environment is ${ENV}`)
 
-DB.init({ 
-    host: Config.get("DB_HOST"),
-    user: Config.get("DB_USER"),
-    password: Config.get("DB_PASSWORD"),
-    dbname: Config.get("DB_NAME"),
-})
+//Config.init(ENV)
+//console.log(Config.all())
+//DB.init({ 
+//    host: Config.get("DB_HOST"),
+//    user: Config.get("DB_USER"),
+//    password: Config.get("DB_PASSWORD"),
+//    dbname: Config.get("DB_NAME"),
+//})
 
-DB.query("SELECT * FROM users")
-    .then(results => {
-        console.log(results)
-    }).catch(e => {
-        console.log(e)
-    })
-
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+//DB.query("SELECT * FROM users")
+//    .then(results => {
+//        console.log(results)
+//    }).catch(e => {
+//        console.log(e)
+//    })
